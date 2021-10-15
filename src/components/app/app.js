80092c590d8f9eced4e37b86a18c3151554d556c
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-10-15 18:55:21 +0300

    likes and mark as important

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,12 @@ import PostAddForm from '../post-add-form'
 import './app.css'
 
 const App = () => {
+    const data = [
+        {label: 'Going to learn React', important: true, id:'qwer'},
+        {label: 'This is so good', important: false, id:'tyui'},
+        {label: 'I need a break...', important: false, id:'opas'},
+    ];
+
     return (
         <div className='app'>
             <AppHeader/>
@@ -14,7 +20,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )    
