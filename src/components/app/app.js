c5a21d084b42482e1216dd8691a9d1c8c7fe6306
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-10-19 21:54:10 +0300

    delete posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import AppHeader from '../app-header'
 import SearchPanel from '../search-panel'
 import PostStatusFilter from '../post-status-filter'
 import PostList from '../post-list'
 import PostAddForm from '../post-add-form'
 import './app.css'
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                {label: 'Going to learn React', important: true, id:'qwer'},
+                {label: 'This is so good', important: false, id:'tyui'},
+                {label: 'I need a break...', important: false, id:'opas'},
+                {label: 'I will be lucky ', important: false, id:'opa'},
+            ]
+        };
+        this.deletItem = this.deletItem.bind(this)
+    }
 
-const App = () => {
-    const data = [
-        {label: 'Going to learn React', important: true, id:'qwer'},
-        {label: 'This is so good', important: false, id:'tyui'},
-        {label: 'I need a break...', important: false, id:'opas'},
-    ];
-
-    return (
-        <div className='app'>
-            <AppHeader/>
-            <div className='search-panel d-flex'>
-                <SearchPanel/>
-                <PostStatusFilter/>
+    deletItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const before = data.splice(0, index);
+            const after = data.splice(index + 1);
+            const newArr = [...before, ...after];
+            return {
+                data: newArr
+            }
+        })
+    }
+ 
+    render() {
+        return (
+            <div className='app'>
+                <AppHeader/>
+                <div className='search-panel d-flex'>
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList posts={this.state.data} onDelete={this.deletItem}/>
+                <PostAddForm/>
             </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-    )    
+        )
+    }    
 }
 
-export default App;
