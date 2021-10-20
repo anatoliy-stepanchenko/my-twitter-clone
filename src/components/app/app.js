18b3b9c28792b7e4760ddfdee9fcf3dac85ebc04
anatoliy-stepanchenko <stepanchenko1991@gmail.com>
2021-10-20 17:13:13 +0300

    add new posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,37 +10,98 @@ export default class App extends Component {
         super(props)
         this.state = {
             data: [
-                {label: 'Going to learn React', important: true, id:'qwer'},
-                {label: 'This is so good', important: false, id:'tyui'},
-                {label: 'I need a break...', important: false, id:'opas'},
-                {label: 'I will be lucky ', important: false, id:'opa'},
+                {label: 'Going to learn React', important: true, like: false, id: 1},
+                {label: 'This is so good', important: false, like: false, id: 2},
+                {label: 'I need a break...', important: false, like: false, id: 3},
+                {label: 'I will be lucky4 ', important: false, like: false, id: 4},
             ]
         };
         this.deletItem = this.deletItem.bind(this)
+        this.addItem = this.addItem.bind(this)
+        this.onToggleImportant = this.onToggleImportant.bind(this)
+        this.onToggleLiked = this.onToggleLiked.bind(this)
+        this.maxId = 5
     }
 
     deletItem(id) {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
-            const before = data.splice(0, index);
-            const after = data.splice(index + 1);
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
             const newArr = [...before, ...after];
             return {
                 data: newArr
             }
         })
     }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return{
+                data: newArr
+            }
+        })
+    }
+
+    onToggleImportant(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = {...old, important: !old.important}
+
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+            const newArr = [...before, newItem, ...after];
+
+            return{
+                data: newArr
+            }
+        })
+    }
+
+    onToggleLiked(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = {...old, like: !old.like}
+
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+            const newArr = [...before, newItem, ...after];
+
+            return{
+                data: newArr
+            }
+        })
+    }
  
     render() {
+        const liked = this.state.data.filter(item => item.like).length;
+        const allPosts = this.state.data.length;
+
         return (
             <div className='app'>
-                <AppHeader/>
+                <AppHeader
+                    liked={liked}
+                    allPosts={allPosts}
+                />
                 <div className='search-panel d-flex'>
                     <SearchPanel/>
                     <PostStatusFilter/>
                 </div>
-                <PostList posts={this.state.data} onDelete={this.deletItem}/>
-                <PostAddForm/>
+                <PostList 
+                posts={this.state.data} 
+                onDelete={this.deletItem}
+                onToggleImportant={this.onToggleImportant}
+                onToggleLiked={this.onToggleLiked}
+                />
+                <PostAddForm onAdd={this.addItem}/>
             </div>
         )
     }    
